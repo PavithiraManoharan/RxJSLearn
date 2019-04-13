1a72893d3904af9ff4b7892d36c64f85bcc5d371
Pavithira Manoharan <pavithira.manoharan@gmail.com>
2019-04-13 21:50:34 +0200

    basic functionality - observable create and subscribe.

--- src/code.ts
+++ src/code.ts
@@ -1,2 +1,25 @@
-import * as Rx from "rxjs";
-console.log(Rx);
+import { Observable } from 'rxjs';
+
+let observable = Observable.create((observer: any) => {
+  try {
+  observer.next('Hello World of Observables!');
+  observer.next('How are you?');
+  observer.complete();
+  observer.next('This will not send');
+} catch(err) {
+  observer.error(err);
+}
+}); //accepts a single argument subscriber
+
+observable.subscribe
+  ((x: any) => addItem(x),
+  (error: any) => addItem(error),
+  () => addItem("Completed")
+);
+
+function addItem(val:any) {
+  var node = document.createElement("li");
+  var textnode = document.createTextNode(val);
+  node.appendChild(textnode);
+  document.getElementById("output").appendChild(node);
+}
